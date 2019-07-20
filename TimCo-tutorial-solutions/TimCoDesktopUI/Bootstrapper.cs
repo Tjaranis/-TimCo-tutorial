9cfe3f2addff7e7307e15e5dac039772b1cc2493
tjaranis <tjaranis@gmail.com>
2019-07-20 05:14:25 +0200

    Added dependency system to Bootstrapper via SimpleContainer and overrides

--- TimCo-tutorial-solutions/TimCoDesktopUI/Bootstrapper.cs
+++ TimCo-tutorial-solutions/TimCoDesktopUI/Bootstrapper.cs
@@ -11,13 +11,50 @@ namespace TimCoDesktopUI
 {
     class Bootstrapper :BootstrapperBase
     {
+        private SimpleContainer _container=new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();  
         }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes() //get all types in the problem
+                .Where(type => type.IsClass) //limit to only classes
+                .Where(type => type.Name.EndsWith("ViewModel")) //limit to only classes which end with "ViewModel"
+                .ToList() //make a list
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType)); //which each entry, get service,key,implementation.
+            //TODO makes it harder to unit test; might change to interfaces later.
+
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
         }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+
     }
 }
